ed0b0726b71b9daea457baf3317dc26128a35240
Vikram-0401 <vikramshettyr4@gmail.com>
2025-02-27 00:43:35 +0530

    added custom select

--- app/(routes)/dashboard/_components/ImageUpload.tsx
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button'
+
 import { CloudUpload } from 'lucide-react'
 import React from 'react'
 
@@ -6,17 +6,21 @@ function ImageUpload() {
   return (
     <div className='mt-10'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-        <div className='p-7 border border-dashed rounded-md shadow-md
-        flex flex-col items-center justify-center
-        '>
-          <CloudUpload className='h-10 w-10' />
-          <h2 className=''> Upload Image</h2>
+      <div className='p-7 border border-dashed rounded-md shadow-md flex flex-col items-center justify-center'>
+        <CloudUpload className='h-10 w-10' />
+        <h2>Upload Image</h2>
 
-          <p className='text-gray-400 mt-3'> Click on select button to upload image </p>
-          <div>
-            <Button className='p-5 border w-full flex mt-7 justify-center'> Select image</Button>
-          </div>
-          </div>
+         <p className='text-gray-400 mt-3'>Click on select button to upload image</p>
+  
+         <div className='p-5 border w-full flex mt-7 justify-center'>
+         <label htmlFor='imageselect' className='cursor-pointer'>
+           <h2 className='p-2 bg-primary text-white rounded-md px-5'>Select Image</h2>
+         </label>
+       </div>
+
+       <input type='file' id='imageselect' className='hidden' />
+       </div>
+
         <div>
           User input textarea
         </div>
